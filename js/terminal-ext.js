faa9e7abe5beb7b74cd79651dd59d632335ecb24
Roger Luo <rogerluo@Rogers-MacBook-Pro-2.local>
2025-05-01 15:13:59 -0700

    Update welcome message

--- js/terminal-ext.js
+++ js/terminal-ext.js
@@ -174,7 +174,7 @@ extend = (term) => {
     term.reset();
     term.printLogoType();
     term.stylePrint(
-      "Welcome to the Embedding VC terminal. An early stage Generative AI investor.",
+      "Welcome to the Embedding VC terminal. We’re an early-stage investor focused on Generative AI startups.",
     );
     term.stylePrint(
       `\r\nType ${colorText("help", "command")} to get started. Or type ${colorText("exit", "command")} for web version.`,
